da9f55e04c0d8d535cfd851affebfd16b7c2c359
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-04-02 11:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ import pickle
 clf = pickle.load(open("avinash_model.sav","rb"))
 
 # Define the sliders for input
-sepal_l = st.slider('Sepal length (cm)', 1.0, 8.0, 0.5)
-sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
-petal_l = st.slider('Petal length (cm)', 1.0, 7.0, 0.5)
-petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
-
-sepal_l = int(sepal_l)
-sepal_w = int(sepal_w)
-petal_l = int(petal_l)
-petal_w = int(petal_w)
+sepal_l = float(st.slider('Sepal length (cm)', 1.0, 8.0, 0.5))
+sepal_w = float(st.slider('Sepal width (cm)', 2.0, 4.4, 0.5))
+petal_l = float(st.slider('Petal length (cm)', 1.0, 7.0, 0.5))
+petal_w = float(st.slider('Petal width (cm)', 0.1, 2.5, 0.5))
 
 # Predict and display the result when the button is clicked
 if st.button("Predict type of Iris"):
