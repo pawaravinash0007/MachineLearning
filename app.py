61f7323e00b8e74495d15d411da8d9cc705c4578
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-04-02 10:54:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,16 @@ import pandas as pd
 import numpy as np
 import pickle
 
+# Load the model
+clf = pickle.load(open("avinash_model.sav","rb"))
 
-def predict(data):
-    clf = pickle.load(open("avinash_model.sav","rb"))
-    return clf.predict(data)
+# Define the sliders for input
+sepal_l = st.slider('Sepal length (cm)', 1.0, 8.0, 0.5)
+sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+petal_l = st.slider('Petal length (cm)', 1.0, 7.0, 0.5)
+petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
 
-st.title('Develope By- Exeternal Guide : Avinash Pawar and WBL Student Team')
-st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into \
-     (setosa, versicolor, virginica) based on their sepal/petal \
-    and length/width.')
-
-st.header("Plant Features")
-col1, col2 = st.columns(2)
-
-with col1:
-    st.text("Sepal characteristics")
-    sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
-
-with col2:
-    st.text("Pepal characteristics")
-    petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
-
-st.text('')
+# Predict and display the result when the button is clicked
 if st.button("Predict type of Iris"):
-    result = predict(
-        np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+    result = clf.predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
     st.text(result[0])
